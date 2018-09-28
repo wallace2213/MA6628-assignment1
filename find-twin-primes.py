85be1fbc3e670a469f05b1427c2abe6c9e0955f8
wallace2213 <42988927+wallace2213@users.noreply.github.com>
2018-09-28 15:09:01 +0800

    update find-twin-prime.py

--- find-twin-primes.py
+++ find-twin-primes.py
@@ -1,5 +1,5 @@
 # find twin prime between 1 thousand to 1 million
-# figure out whether a integer is a prime number
+# figure out whether an integer is a prime number
 def isPrime(n):                          
     if n == 2 or n == 3: return True     # 2 and 3 are prime numbers
     if n%2==0 or n<2: return False       # even numbers are not prime numbers
@@ -7,10 +7,10 @@ def isPrime(n):
         if n%i==0:                       # if a number can be divided by another number in the range of 3 to root n, 
             return False                 # it is not a prime number
     return True
-# figure out whether twin integers are twin primes
+# figure out whether two integers are twin primes
 def isTwinPrime(n):
     if isPrime(n) and isPrime(n+2): return True   # if n and n+2 are both prime numbers, they are twin prime
-# find out the total number and the maxmimum twim primes between 1k and 1m
+# find out the total number and the maxmimum twin primes between 1k and 1m
 j=0
 import numpy as np
 x=np.zeros(1000000)
@@ -18,11 +18,11 @@ for i in range(1000,1000000):
     if isTwinPrime(i):
         x[j]=i                            # x[j] stores the smaller one of the twin prime
         j=j+1                             # j is the total number of twin prime
-print('number of twim primes=',j,'maxmimum twim primes=', x[j-1], x[j-1]+2,sep='\n' )
+print('number of twin primes=',j,'maxmimum twin primes=', x[j-1], x[j-1]+2,sep='\n' )
 
 # result and output
-# number of twim primes=
+# number of twin primes=
 # 8134
-# maxmimum twim primes=
+# maxmimum twin primes=
 # 999959.0
 # 999961.0
